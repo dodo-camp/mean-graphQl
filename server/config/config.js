d4a9460c541e4aa147bc083d3b2ceb2e6d0c475a
Dhruv Mukherjee <mukherjeedhruv1@gmail.com>
2018-12-21 15:26:45 +0530

    update

--- server/config/config.js
+++ server/config/config.js
@@ -5,18 +5,14 @@ module.exports = () => ({
     mongo: {
         uri: "mongodb://test_app:addodo1996oct1@ds227332.mlab.com:27332/dodo"
     },
-    session: (MongoStore) => ({
-        secret: 'super_dodo',
-        key: 'express.sid',
-        sessionKey: 'application_dodo',
-        name: 'cookie',
+    session: (MemcachedStore) => ({
+        secret: 'ClydeIsASquirrel',
+        resave: 'false',
+        saveUninitialized: 'false',
         cookie: { path: '/', maxAge: 60 * 60 * 1000 },
-        resave: false,
-        saveUninitialized: false,
-        clientSecret: 'QEQWE@#4234234ASDASDCZXC__+++2123123',
-        store: new MongoStore({
-            url: 'mongodb://test_app:addodo1996oct1@ds227332.mlab.com:27332/dodo',
-            ttl: 60 * 60 * 1000
+        store: new MemcachedStore({
+            servers: [process.env.MEMCACHIER_SERVERS],
+            prefix: '_session_'
         })
     })
 });
