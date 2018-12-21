0e0488ff84029fa9c6c822ba5dc363312010b0f1
Dhruv Mukherjee <mukherjeedhruv1@gmail.com>
2018-12-21 13:13:41 +0530

    update

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = () => ({
     app: {
-        port: process.env.APP_PORT || 3000
+        port: process.env.PORT || 3000
     },
     mongo: {
         uri: "mongodb://test_app:addodo1996oct1@ds227332.mlab.com:27332/dodo"
